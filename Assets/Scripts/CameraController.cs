fd50eb566077f6b98d8d35326e8b34bd45e65bcf
witt3rd <witt3rd@witt3rd.com>
2019-09-07 02:17:54 -0700

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+    
+    public float pitch = 2f;
+    public float yawSpeed = 100f;
+        
+    private float _currentZoom = 10f;
+    private float _currentYaw = 0f;
+    
+    private void Update()
+    {
+        _currentZoom += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        _currentZoom = Mathf.Clamp(_currentZoom, minZoom, maxZoom);
+
+        _currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+        var position = target.position;
+        transform.position = position - offset * _currentZoom;
+        transform.LookAt(position + Vector3.up * pitch);
+        transform.RotateAround(position, Vector3.up, _currentYaw);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    
+    private Camera cam;
+
+    private PlayerMotor motor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    cam = Camera.main;
+    motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        // Move on left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out var hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                // Defocus
+            }
+        }
+
+        // Focus on right click
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Physics.Raycast(ray, out var hit, 100))
+            {
+                // Interactable?
+            }
+        }
+    }
+}

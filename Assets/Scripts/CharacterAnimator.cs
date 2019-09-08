f24119562e34b7cc9ea1414844a3885cc6f65b56
witt3rd <witt3rd@witt3rd.com>
2019-09-08 04:22:19 -0700

    E03 - player character animation rig

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public float dampTime = .1f;
+    
+    private NavMeshAgent _agent;
+    private Animator _animator;
+    
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponentInChildren<Animator>();
+
+    }
+
+    void Update()
+    {
+        var speedPercent = _agent.velocity.magnitude / _agent.speed;
+        _animator.SetFloat("speedPercent", speedPercent, dampTime, Time.deltaTime);
+    }
+}
